041075b7a29a4ade64d4bcf84c6576ac6887d7ac
Larry Adams <thewitness@cacti.net>
2010-07-31 16:52:24 +0000

    bug#0001799: Select all checkbox on Mac Addresses tab not working
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti_plugins/mactrack/trunk@1163 67214365-cc29-0410-bfbe-843132f0f3d7

--- general_header.php
+++ general_header.php
@@ -62,6 +62,7 @@ $page_title = api_plugin_hook_function('page_title', 'Cacti');
 	?>
 	<link href='<?php echo $config["url_path"]; ?>include/main.css' rel='stylesheet'>
 	<link href='<?php echo $config["url_path"]; ?>images/favicon.ico' rel='shortcut icon'>
+	<script type='text/javascript' src='<?php echo $config["url_path"]; ?>include/layout.js'></script>
 	<script type='text/javascript' src='<?php echo $config["url_path"]; ?>plugins/mactrack/jquery.js'></script>
 	<script type='text/javascript' src='<?php echo $config["url_path"]; ?>include/jscalendar/calendar.js'></script>
 	<script type='text/javascript' src='<?php echo $config["url_path"]; ?>include/jscalendar/lang/calendar-en.js'></script>
