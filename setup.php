9c60d1821bef84ec44ea9a5c8210ccae942291f9
TheWitness <thewitness@cacti.net>
2022-03-19 07:20:37 -0400

    QA: MacTrack polluting generalized REQUEST space

--- setup.php
+++ setup.php
@@ -842,7 +842,6 @@ function mactrack_show_tab() {
 	include_once($config['base_path'] . '/plugins/mactrack/lib/mactrack_functions.php');
 
 	if (api_user_realm_auth('mactrack_view_macs.php')) {
-		mactrack_sanitize_load_report();
 		if (substr_count($_SERVER['REQUEST_URI'], 'mactrack_view_')) {
 			print '<a href="' . html_escape($config['url_path'] . 'plugins/mactrack/mactrack_view_' . get_request_var('report') . '.php') . '"><img src="' . $config['url_path'] . 'plugins/mactrack/images/tab_mactrack_down.png" alt="' . __('MacTrack', 'mactrack') . '"></a>';
 		} else {
